8893bd464c52180f3201cbd04d08420afd01570f
Abikesh Shrestha <shresthaabks@gmail.com>
2026-05-22 21:54:26 -0700

    Add notes app with localStorage

--- notes-app/index.js
+++ notes-app/index.js
@@ -1,11 +1,47 @@
 const notes = document.getElementById("notes");
 const button = document.getElementById("myBtn");
 let divNotes = document.getElementById("newNotes");
+const storedData = localStorage.getItem("notes");
+const notesArray = JSON.parse(storedData);
 let counter = 0;
+let notesData = JSON.parse(storedData) || [];
+
+
+console.log(notesArray)
+
+for (const data of notesData)
+{
+  counter++;
+
+  let newNotes = document.createElement('div');
+
+  newNotes.innerHTML = data;
+
+  let deleteBtn = document.createElement('button');
+  let notesTitle = document.createElement('h2');
+  newNotes.className = 'newNotes';
+  deleteBtn.textContent = 'delete'
+
+  deleteBtn.id = 'deleteButton';
+  deleteBtn.textContent = 'delete'
+  deleteBtn.addEventListener('click', function removeNotes()
+{
+  newNotes.remove();
+  deleteBtn.remove();
+  counter--;
+})
+
+   divNotes.appendChild(newNotes);
+  newNotes.appendChild(deleteBtn);
+   newNotes.appendChild(notesTitle);
+
+  notesTitle.textContent = `Note ${counter}`
+}
 
 button.addEventListener("click", function add()
 {
   counter++;
+  let noteText = notes.value;
   let notesTitle = document.createElement('h2');
   let newNotes = document.createElement('div');
   let deleteBtn = document.createElement('button');
@@ -29,7 +65,12 @@ button.addEventListener("click", function add()
   newNotes.appendChild(deleteBtn);
    newNotes.appendChild(notesTitle);
   
+   notesData.push(noteText);
+ 
 
+console.log(notesData)
 
+localStorage.setItem("notes", JSON.stringify(notesData));
 })
+
 
