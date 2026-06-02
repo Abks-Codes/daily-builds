75b1250d55172c5634d2d1b84d105c81420555a0
Abikesh Shrestha <shresthaabks@gmail.com>
2026-06-01 23:14:11 -0700

    Add persistent favorites to movie finder

--- Movie Search App/index.js
+++ Movie Search App/index.js
@@ -6,7 +6,44 @@ let loading = document.getElementById("loading");
 let cards = document.querySelector('.cards');
 let notfound = document.getElementById("notFound");
 const favButton = document.getElementById("toggle-favorites");
-let favDiv = document.getElementById("favorites")
+let favDiv = document.getElementById("favorites");
+const savedData = localStorage.getItem('userFavorites');
+let favoritess = JSON.parse(savedData) || [];
+
+for(let cardi of favoritess)
+{
+     
+        let favCard = document.createElement("div");
+        favCard.className = 'card';
+        let title = document.createElement("h2");
+        let year = document.createElement("h3");
+        let rated = document.createElement("h3");
+        let released = document.createElement("h3");
+        let genre = document.createElement("h3");
+        let type = document.createElement("h3");
+        let poster = document.createElement("img");
+
+        title.textContent = `Title : ${cardi.Title}`;
+        year.textContent = `Year: ${cardi.Year}`;
+        rated.textContent = `Rated: ${cardi.Rated}`;
+        released.textContent = `Released: ${cardi.Released}`;
+        genre.textContent = `Genre: ${cardi.Genre}`;
+        type.textContent = `Type: ${cardi.Type}`;
+        poster.src = cardi.Poster;
+        poster.className = 'poster';
+
+         favCard.appendChild(title);
+        favCard.appendChild(year);
+        favCard.appendChild(rated);
+        favCard.appendChild(released);
+        favCard.appendChild(genre);
+        favCard.appendChild(type);
+        favCard.appendChild(poster);
+       favDiv.appendChild(favCard);
+
+
+
+}
 
 
 button.addEventListener("click", async function getMovie()
@@ -22,12 +59,9 @@ button.addEventListener("click", async function getMovie()
     let response = await fetch(`https://www.omdbapi.com/?apikey=[your-api]&s=${input.value}`);
     const data = await response.json();
     
-    if(data.response){
-        throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    else{
-        console.log("works")
-    }  
+         if (data.Response === "False") { 
+            throw new Error(data.Error || "Movie not found"); 
+        } 
 
 
    
@@ -85,14 +119,19 @@ button.addEventListener("click", async function getMovie()
                  
                  addtofavorite.classList.remove("fa-solid");
                  addtofavorite.classList.add("fa-regular");
-                 favoriteCard.remove()
+                 favoriteCard.remove();
+                
             
                 console.log("hoina");
 
             }
             else
             {
-            
+              let favoriteMovie = {Title : movie.Title, Year : movie.Year, Rated : more_data.Rated, Released : more_data.Released, Genre : more_data.Genre, Type : movie.Type, Poster : more_data.Poster};
+                favoritess.push(favoriteMovie);
+        
+                localStorage.setItem('userFavorites', JSON.stringify(favoritess));
+
                 
                 addtofavorite.classList.add("fa-solid");
                 favoriteCard = document.createElement("div");
@@ -100,6 +139,8 @@ button.addEventListener("click", async function getMovie()
                 favoriteCard.innerHTML = card.innerHTML;
                 favDiv.appendChild(favoriteCard)
                 addtofavorite.classList.remove("fa-regular");
+
+                
                 
 
                 console.log("hora")
@@ -129,9 +170,9 @@ button.addEventListener("click", async function getMovie()
  }
     
   
-})
+});
 
-
+ 
 
 favButton.addEventListener('click', function toggleFavorites(){
  if(favDiv.style.display == 'none')
